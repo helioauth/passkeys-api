250f4bbbc94177a94c2dd41f1db890361de41920
Victor Stanchev <st.victor@gmail.com>
2024-08-27 10:05:53 +0300

    Added user and credential models

---
+++ src/main/java/com/helioauth/passkeys/api/domain/User.java
@@ -0,0 +1,43 @@
+package com.helioauth.passkeys.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "demo_users")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String displayName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<UserCredential> userCredentials;
+
+    protected User() {
+
+    }
+}
---
+++ src/main/java/com/helioauth/passkeys/api/domain/UserCredential.java
@@ -0,0 +1,69 @@
+package com.helioauth.passkeys.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.Instant;
+
+@Entity
+@Table(name = "demo_registered_credential")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserCredential {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String credentialId;
+
+    @Column
+    private String userHandle;
+
+    @Column
+    private String displayName;
+
+    @Column
+    private Long signatureCount;
+
+    @Column(columnDefinition = "text")
+    private String publicKeyCose;
+
+    @Column(columnDefinition = "text")
+    private String attestationObject;
+
+    @Column(columnDefinition = "text")
+    private String clientDataJson;
+
+    @Column
+    private Boolean backupEligible;
+
+    @Column
+    private Boolean backupState;
+
+    @Column
+    private Boolean isDiscoverable;
+
+    @Column
+    private Instant lastUsedAt;
+
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    protected UserCredential() {
+
+    }
+}
