0f9e7248ab21ea778292e2779d346d118b9c678f
Victor Stanchev <st.victor@gmail.com>
2024-08-29 18:32:51 +0300

    Add copyright and author information

--- src/main/java/com/helioauth/passkeys/api/service/WebAuthnAuthenticator.java
+++ src/main/java/com/helioauth/passkeys/api/service/WebAuthnAuthenticator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.helioauth.passkeys.api.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,6 +40,9 @@ import java.time.Instant;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
+/**
+ * @author Viktor Stanchev
+ */
 @Slf4j
 @Service
 @RequiredArgsConstructor
--- src/main/java/com/helioauth/passkeys/api/webauthn/UserCredentialRegisteredCredentialMapper.java
+++ src/main/java/com/helioauth/passkeys/api/webauthn/UserCredentialRegisteredCredentialMapper.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.helioauth.passkeys.api.webauthn;
 
 import com.helioauth.passkeys.api.domain.UserCredential;
@@ -9,6 +25,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * @author Viktor Stanchev
+ */
 @Component
 public class UserCredentialRegisteredCredentialMapper {
 
