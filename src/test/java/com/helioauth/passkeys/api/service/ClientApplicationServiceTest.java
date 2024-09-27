832cf05f9f0238043031fc88dd4fef48bc3bb48c
Victor Stanchev <st.victor@gmail.com>
2024-09-27 17:28:36 +0300

    Add tests for ClientApplicationService.

---
+++ src/test/java/com/helioauth/passkeys/api/service/ClientApplicationServiceTest.java
@@ -0,0 +1,156 @@
+package com.helioauth.passkeys.api.service;
+
+import com.helioauth.passkeys.api.contract.AddClientApplicationRequest;
+import com.helioauth.passkeys.api.domain.ClientApplication;
+import com.helioauth.passkeys.api.mapper.ClientApplicationMapper;
+import com.helioauth.passkeys.api.service.dto.ClientApplicationApiKeyDTO;
+import com.helioauth.passkeys.api.service.dto.ClientApplicationDTO;
+import com.helioauth.passkeys.api.domain.ClientApplicationRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientApplicationServiceTest {
+
+    public static final ClientApplicationDTO DTO = new ClientApplicationDTO(
+        UUID.randomUUID(),
+        "App Name",
+        Instant.now(),
+        Instant.now()
+    );
+
+    public static final ClientApplication CLIENT_APPLICATION = ClientApplication.builder()
+        .id(DTO.id())
+        .name(DTO.name())
+        .createdAt(DTO.createdAt())
+        .updatedAt(DTO.updatedAt())
+        .build();
+
+    @Mock
+    private ClientApplicationRepository repository;
+
+    @Spy
+    private ClientApplicationMapper mapper = Mappers.getMapper( ClientApplicationMapper.class);
+
+    @InjectMocks
+    private ClientApplicationService service;
+
+    @Test
+    public void addClientApplicationTest() {
+        // Setup
+        when(repository.save(any(ClientApplication.class))).thenReturn(CLIENT_APPLICATION);
+
+        // Execute
+        ClientApplicationDTO result = service.add(new AddClientApplicationRequest(DTO.name()));
+
+        // Validate
+        assertEquals(DTO.name(), result.name());
+    }
+
+    @Test
+    public void listAllClientApplicationsTest() {
+        // Setup
+        List<ClientApplication> applications = List.of(CLIENT_APPLICATION);
+        when(repository.findAll()).thenReturn(applications);
+
+        // Execute
+        List<ClientApplicationDTO> result = service.listAll();
+
+        // Validate
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(DTO.id(), result.getFirst().id());
+        assertEquals(DTO.name(), result.getFirst().name());
+    }
+
+    @Test
+    public void editClientApplicationTest() {
+        // Setup
+        UUID id = UUID.randomUUID();
+        String newName = "Updated Name";
+        ClientApplication existingClientApplication = ClientApplication.builder()
+            .id(id)
+            .name("Old Name")
+            .createdAt(Instant.now())
+            .updatedAt(Instant.now())
+            .build();
+
+        when(repository.findById(id)).thenReturn(Optional.of(existingClientApplication));
+        when(repository.save(any(ClientApplication.class))).thenReturn(existingClientApplication);
+
+        // Execute
+        Optional<ClientApplicationDTO> result = service.edit(id, newName);
+
+        // Validate
+        assertTrue(result.isPresent());
+        assertEquals(newName, result.get().name());
+    }
+
+    @Test
+    public void deleteClientApplicationTest() {
+        // Setup
+        UUID id = UUID.randomUUID();
+        when(repository.existsById(id)).thenReturn(true);
+
+        // Execute
+        boolean result = service.delete(id);
+
+        // Validate
+        assertTrue(result);
+        when(repository.existsById(id)).thenReturn(false);
+        result = service.delete(id);
+        assertFalse(result);
+    }
+
+    @Test
+    public void getClientApplicationTest() {
+        // Setup
+        UUID id = DTO.id();
+        when(repository.findById(id)).thenReturn(Optional.of(CLIENT_APPLICATION));
+
+        // Execute
+        Optional<ClientApplicationDTO> result = service.get(id);
+
+        // Validate
+        assertTrue(result.isPresent());
+        assertEquals(DTO.id(), result.get().id());
+        assertEquals(DTO.name(), result.get().name());
+    }
+
+    @Test
+    public void getClientApplicationApiKeyTest() {
+        // Setup
+        UUID id = UUID.randomUUID();
+        String apiKey = "sampleApiKey";
+        ClientApplication clientApplicationWithApiKey = ClientApplication.builder()
+            .id(id)
+            .name("App Name")
+            .apiKey(apiKey)
+            .createdAt(Instant.now())
+            .updatedAt(Instant.now())
+            .build();
+
+        when(repository.findById(id)).thenReturn(Optional.of(clientApplicationWithApiKey));
+
+        // Execute
+        Optional<ClientApplicationApiKeyDTO> result = service.getApiKey(id);
+
+        // Validate
+        assertTrue(result.isPresent());
+        assertEquals(apiKey, result.get().apiKey());
+    }
+}
