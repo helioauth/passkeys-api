f7239e8ea04f7d756edbb217cc3dd28a242089d1
Victor Stanchev <st.victor@gmail.com>
2024-09-29 19:54:06 +0300

    Set created and updated dates automatically

--- src/main/java/com/helioauth/passkeys/api/PasskeysApiApplication.java
+++ src/main/java/com/helioauth/passkeys/api/PasskeysApiApplication.java
@@ -19,12 +19,14 @@ package com.helioauth.passkeys.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author Viktor Stanchev
  */
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 public class PasskeysApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/helioauth/passkeys/api/domain/ClientApplication.java
+++ src/main/java/com/helioauth/passkeys/api/domain/ClientApplication.java
@@ -24,14 +24,18 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -41,6 +45,7 @@ import java.util.UUID;
  */
 @Entity
 @Table(name = "applications")
+@EntityListeners(AuditingEntityListener.class)
 @Builder
 @Getter
 @Setter
@@ -59,10 +64,12 @@ public class ClientApplication {
     private String apiKey;
 
     @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", nullable = false)
     private Instant createdAt;
 
     @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated_at", nullable = false)
     private Instant updatedAt;
 
