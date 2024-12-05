a98a3f26f971a181cb3a2c42cb8974b29358a10b
Victor Stanchev <st.victor@gmail.com>
2024-12-05 06:44:49 +0200

    Remove redundant request mapping annotations

--- src/main/java/com/helioauth/passkeys/api/controller/ClientApplicationController.java
+++ src/main/java/com/helioauth/passkeys/api/controller/ClientApplicationController.java
@@ -24,13 +24,8 @@ import com.helioauth.passkeys.api.service.ClientApplicationService;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -41,36 +36,27 @@ import java.util.UUID;
  * @author Viktor Stanchev
  */
 @RestController
-@RequestMapping("/admin/v1/apps")
 @RequiredArgsConstructor
 public class ClientApplicationController implements ApplicationsApi {
 
     private final ClientApplicationService clientApplicationService;
 
-    @GetMapping
-    @Override
     public ResponseEntity<List<Application>> listAll() {
         return ResponseEntity.ok(clientApplicationService.listAll());
     }
 
-    @GetMapping("/{id}")
-    @Override
     public ResponseEntity<Application> get(@PathVariable UUID id) {
         return clientApplicationService.get(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/{id}/api-key")
-    @Override
     public ResponseEntity<ApplicationApiKey> getApiKey(@PathVariable UUID id) {
         return clientApplicationService.getApiKey(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @PostMapping
-    @Override
     public ResponseEntity<Application> add(@RequestBody AddApplicationRequest request) {
         Application created = clientApplicationService.add(request.getName());
 
@@ -78,10 +64,7 @@ public class ClientApplicationController implements ApplicationsApi {
             .body(created);
     }
 
-    @PutMapping("/{id}")
-    @Override
     public ResponseEntity<Application> edit(@PathVariable UUID id, @RequestBody String name) {
-
         val updated = clientApplicationService.edit(id, name);
 
         return updated
@@ -89,8 +72,6 @@ public class ClientApplicationController implements ApplicationsApi {
             .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @DeleteMapping("/{id}")
-    @Override
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         boolean deleted = clientApplicationService.delete(id);
 
